26f56918e95c7f13674f3372a7f8f0c2632574d2
Илья Б <bobrenev1545@gmail.com>
2022-05-27 12:10:29 +0700

    added parse article

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
+let service = document.querySelector(".servis");
 console.log("консоль работает");
 
-fetch("https://fontan.city/services/").then(response =>response.text()).then(html => {
-        let parser = new DOMParser(); // Парсим полученынй документ из строки в HTML
-        let docParse = parser.parseFromString(html, 'text/html');
-        console.log(docParse);
+document.querySelector('button').onclick = function(){
 
-        let services = docParse.querySelectorAll(".sect > a");
-        let arrServ = Array.from(services).map(el => 'https://fontan.city/' + el.href.split("/").slice(el.href.split("/").indexOf("services")).join('/'));
-        arrServ.map(el=>{
-            fetch(el).then(response => response.text()).then(html => {
-                let parser = new DOMParser(); // Парсим полученынй документ из строки в HTML
-                let docParse = parser.parseFromString(html, 'text/html');
+    let list_url = [
+        {url:"https://fontan.city/services/",chapter:"services", class:"sect"},
+        {url:"https://fontan.city/tipovye-resheniya/",chapter:"tipovye-resheniya", class:"sect"},
+        {url:"https://fontan.city/advice/?PAGEN_1=1",chapter:"advice", class:"inner-item  > .title"},
+        {url:"https://fontan.city/advice/?PAGEN_1=2",chapter:"advice", class:"inner-item  > .title"},
+        {url:"https://fontan.city/advice/?PAGEN_1=3",chapter:"advice", class:"inner-item  > .title"},
+        {url:"https://fontan.city/advice/?PAGEN_1=4",chapter:"advice", class:"inner-item  > .title"},
+        {url:"https://fontan.city/advice/?PAGEN_1=5",chapter:"advice", class:"inner-item  > .title"},
+        {url:"https://fontan.city/advice/?PAGEN_1=6",chapter:"advice", class:"inner-item  > .title"},
+        {url:"https://fontan.city/advice/?PAGEN_1=7",chapter:"advice", class:"inner-item  > .title"},
+        {url:"https://fontan.city/advice/?PAGEN_1=8",chapter:"advice", class:"inner-item  > .title"},
+    ]
 
-                let sc = docParse.querySelector('sc');
-                console.log(sc);
+let count = 0;
+service.innerHTML = "";
+
+
+    // Перебераем массив ссылок
+    list_url.map(elementList => fetch(elementList.url).then(response =>response.text()).then(html => {
+            let parser = new DOMParser();                                       // Парсим полученынй документ из строки в HTML
+            let docParse = parser.parseFromString(html, 'text/html');
+
+            let services = docParse.querySelectorAll(`.${elementList.class} > a`);
+
+            let arrServ = Array.from(services).map(el => 'https://fontan.city/' + el.href.split("/").slice(el.href.split("/").indexOf(elementList.chapter)).join('/'));
+            
+            
+            // Проверка испранвости скрипта на страничке
+            arrServ.map(el=>{
+
+                fetch(el).then(response => response.text()).then(html => {
+                    let parser = new DOMParser();
+                    let docParse = parser.parseFromString(html, 'text/html');
+
+                    let sc = docParse.querySelector('sc');
+                    if(sc) {
+                        let h1Services = docParse.querySelector('#pagetitle');
+                        service.innerHTML += `${++count}) <a href=${el}><span id = "url_name">${h1Services.textContent}</span></a></br>`;
+                        console.log(sc);
+                    }
+                })
             })
         })
-        console.log(arrServ);
-        })
-        
-       
-        
-
-
-        
+    )
+}
